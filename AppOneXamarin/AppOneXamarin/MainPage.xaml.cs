3893025d2d26e8001bb533221ead0d7c5e7e6e0e
KingDarBoja <mbojato@uninorte.edu.co>
2017-11-13 18:14:30 -0500

    Agregar archivos de proyecto.

---
+++ AppOneXamarin/AppOneXamarin/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppOneXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            // InitializeComponent();
+            var layout = new StackLayout { Margin = 20 };
+            this.Content = layout;
+            var button = new Button
+            {
+                Text = "El Botón",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            button.Clicked += OnButtonClicked;
+            var label = new Label { Text = "Hola, presiona el botón" };
+            layout.Children.Add(label);
+            layout.Children.Add(button);
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            DisplayAlert(" Alerta ", " Bienvenido ", "OK");
+        }
+    }
+}
